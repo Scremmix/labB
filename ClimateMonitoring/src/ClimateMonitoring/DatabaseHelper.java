f8818ad6dea163d8d50c797ebd40f86825b7d4b5
Alessandro Scremin <57624331+Scremmix@users.noreply.github.com>
2024-06-25 19:33:23 +0200

    Creata classe DatabaseHelper, che interagisce direttamente con 	il database di PostgreSQL TODO: - aggiungere tabella per N:N tra centri e località 	  - aggiungere funzioni per ottenere i dati richiesti 	  - completare la cancellazione delle tabelle

---
+++ ClimateMonitoring/src/ClimateMonitoring/DatabaseHelper.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ClimateMonitoring;
+
+import java.sql.*;
+
+/**
+ *
+ * @author alesc
+ */
+public class DatabaseHelper 
+{
+    private String url="";
+    private String user="";
+    private String password="";
+    private Connection connection=null;
+
+    public DatabaseHelper(String url, String user, String password) {
+        this.url=url;
+        this.user=user;
+        this.password=password;
+    }
+    
+    public boolean getConnection()
+    {
+        try{
+            connection = DriverManager.getConnection("jdbc:postgresql://"+url,user,password);
+            if(connection!=null)
+            {
+                System.out.println("Connessione al database PostgreSQL effettuata con successo.");
+                return true;
+            }
+            else
+            {
+                System.err.println("Connessione al database PostgreSQL fallita.");
+                return false;
+            }
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public boolean databaseInit()
+    {
+        try{
+            Statement stmt = connection.createStatement();
+            String currentQuery = "DROP TABLE CentroMonitoraggio IF EXISTS;"
+                    + "DROP TABLE CoordinateMonitoraggio IF EXISTS;";
+            stmt.executeUpdate(currentQuery);
+            
+            
+            currentQuery =  "CREATE TABLE CentroMonitoraggio ( "
+                    + "idCentro INTEGER PRIMARY KEY, "
+                    + "nomeCentro TEXT, "
+                    + "indirizzoCentro TEXT, "
+                    + "capCentro INTEGER, "
+                    + "cittaCentro TEXT, "
+                    + "statoCentro TEXT );";
+            stmt.executeUpdate(currentQuery);
+            
+            currentQuery = "CREATE TABLE CoordinateMonitoraggio("
+                    + "idLocalita INTEGER PRIMARY KEY, "
+                    + "nomeLocalita TEXT, "
+                    + "statoLocalita TEXT, "
+                    + "latitudineLocalita NUMERIC, "
+                    + "longitudineLocalita NUMERIC);";
+            stmt.executeUpdate(currentQuery);
+            
+            currentQuery = "CREATE TABLE OperatoriRegistrati("
+                    + "nomeOperatore TEXT, "
+                    + "cognomeOperatore TEXT, "
+                    + "cfOperatore TEXT, "
+                    + "mailOperatore TEXT, "
+                    + "codiceOperatore INTEGER PRIMARY KEY, "
+                    + "passwordOperatore TEXT NOT NULL, "
+                    + "centroOperatore INTEGER, "
+                    + "FOREIGN KEY (centroOperatore) REFERENCES CentroMonitoraggio(idCentro) ON DELETE CASCADE ON UPDATE CASCADE);";
+            stmt.executeUpdate(currentQuery);
+            
+            currentQuery = "CREATE TABLE ParametriClimatici("
+                    + "idRegistrazione SERIAL PRIMARY KEY, "
+                    + "centroRegistrazione INTEGER, "
+                    + "localitaRegistrazione INTEGER, "
+                    + "ventoParametro INTEGER, "
+                    + "umiditaParametro INTEGER, "
+                    + "precatmParametro INTEGER, "
+                    + "tempParametro INTEGER, "
+                    + "precipParametro INTEGER, "
+                    + "altghiParametro INTEGER, "
+                    + "massaghiParametro INTEGER, "
+                    + "noteRegistrazione TEXT, "
+                    + "FOREIGN KEY (centroRegistrazione) REFERENCES CentroMonitoraggio(idCentro) ON DELETE CASCADE ON UPDATE CASCADE, "
+                    + "FOREIGN KEY (localitaRegistrazone) REFERENCES CoordinateMonitoraggio(idLocalita) ON DELETE CASCADE ON UPDATE CASCADE);";
+            stmt.executeUpdate(currentQuery);
+            
+            stmt.close();
+            return true;
+        } catch (SQLException ex) {
+            System.err.println("Errore imprevisto nella connessione al database");
+            return false;
+        }
+    }
+}
--- ClimateMonitoring/src/ClimateMonitoring/ServerCM.java
+++ ClimateMonitoring/src/ClimateMonitoring/ServerCM.java
@@ -3,6 +3,7 @@ package ClimateMonitoring;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.rmi.registry.*;
+import java.sql.*;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -18,14 +19,26 @@ public class ServerCM extends UnicastRemoteObject implements ServerCMInterface
     {
         super();
     }
+    // esempio di parametri:
+    // localhost/postgres ClimateMonitoring DatabaseCM 
     public static void main(String args[])
     {
-        try {
-            Registry r = LocateRegistry.getRegistry();
-            r.rebind("ServerCM", new ServerCM());
-            System.out.println("ServerCM : pronto per ricevere richieste.");
-        } catch (RemoteException ex) {
-            System.err.println("ServerCM: impossibile inizializzare la connessione.");
+        if(args.length == 3)
+        {
+            DatabaseHelper dbh = new DatabaseHelper(args[0],args[1],args[2]);
+            if(dbh.getConnection())
+                try {
+                    Registry r = LocateRegistry.getRegistry();
+                    r.rebind("ServerCM", new ServerCM());
+                    System.out.println("ServerCM : pronto per ricevere richieste.");
+                } catch (RemoteException ex) {
+                    System.err.println("ServerCM: impossibile inizializzare la connessione.");
+                }
+            else{
+                System.err.println("Sono richiesti i seguenti parametri: ");
+                System.err.println("[url del database] [username] [password]");
+                System.err.println("Esempio di utilizzo: localhost/postgres ClimateMonitoring DatabaseCM");
+            }
         }
     }
 }
